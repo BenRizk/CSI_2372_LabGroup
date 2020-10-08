f19849357928d42ec8bef315cfa4c07c0c1a2562
BenRizk <coolguyben@hotmail.com>
2020-10-08 15:05:28 -0400

    Exercise 1 a) is complete
    Finished 1a)

--- Lab3/Exercise1/a)/myFile1a/myFile1a.cpp
+++ Lab3/Exercise1/a)/myFile1a/myFile1a.cpp
@@ -13,9 +13,54 @@ struct Card
 
 int main()
 {
+	Card temp;
+	
+	int hold= 0;
+	int hold2 = 0;
+
 	Card game[32];
 	for (int i = 0; i < 32; i++) {
-
+		hold = rand() % 4;		//choose random number from 0-3
+		hold2 = rand() % 7;		//choose random number from 0-7
+		//decide suit
+		if (hold == 0) {
+			game[i].color = club;
+		}
+		if (hold == 1) {
+			game[i].color = diamond;
+		}
+		if (hold == 2) {
+			game[i].color = spades;
+		}
+		if (hold == 3) {
+			game[i].color = heart;
+		}
+		//decide face
+		if (hold2 == 0) {
+			game[i].face = seven;
+		}
+		if (hold2 == 1) {
+			game[i].face = eight;
+		}
+		if (hold2 == 2) {
+			game[i].face = nine;
+		}
+		if (hold2 == 3) {
+			game[i].face = ten;
+		}
+		if (hold2 == 4) {
+			game[i].face = jack;
+		}
+		if (hold2 == 5) {
+			game[i].face = queen;
+		}
+		if (hold2 == 6) {
+			game[i].face = king;
+		}
+		if (hold2 == 7) {
+			game[i].face = ace;
+		}
+		
 	}
 }
 
