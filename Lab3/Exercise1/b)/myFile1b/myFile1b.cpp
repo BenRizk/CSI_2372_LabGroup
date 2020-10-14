628268060414316c08173eb1542057cf465093ba
BenRizk <coolguyben@hotmail.com>
2020-10-14 19:04:12 -0400

    Exercise 1 finished
    part b) was completed

--- Lab3/Exercise1/b)/myFile1b/myFile1b.cpp
+++ Lab3/Exercise1/b)/myFile1b/myFile1b.cpp
@@ -28,18 +28,21 @@ bool testPair(const Hand& h)
 /*Example of main()*/
 int main()
 {
+	Card first;
+	first.color = club;
+	first.face = seven;
+	Card second;
+	second.color = spades;
+	second.face = ten;
+	Card third;
+	third.color = heart;
+	third.face = ace;
+	Card fourth;
+	fourth.color = club;
+	fourth.face = queen;
+	Hand myHand = {first, second, third, second, fourth};
 	bool testPair(const Hand&);
-	Hand myHand = {};
-	cout << "\nI have at least:" << testPair(myHand) << "pair" << endl;
+	
+	cout << "\nI have at least: " << testPair(myHand) << " pair" << endl;
 	return 0;
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
